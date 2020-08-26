dad1deeea2968b2b2e6ad92db5cc4ad704320831
Patrick-G-Oliver <patrickgriffitholiver@gmail.com>
2020-08-25 20:47:16 -0400

    ACTIONS_ADDED

---
+++ app/src/actions/index.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+export const FETCHING_PHOTO_START = "FETCHING_PHOTO-START";
+export const FETCHING_PHOTO_SUCCESS = "FETCHING_PHOTO-SUCCESS";
+export const FETCHING_PHOTO_FAILURE = "FETCHING_PHOTO-FAILURE";
+
+export const getPhoto = () => dispatch => {
+    dispatch({ type: "FETCHING_PHOTO_START" });
+    axios 
+        .get("https://picsum.photos/200/300")
+        .then(res => {
+            console.log(res);
+            dispatch({ type: FETCHING_PHOTO_SUCCESS, payload: res.data.message })
+        })
+        .catch(err => {
+            console.log(err);
+            dispatch({
+                type: FETCHING_IMAGE_FAILURE,
+                payload: err.response.message
+            });
+        });
+};
